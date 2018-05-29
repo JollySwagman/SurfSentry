0c3ff488d5f2ef02dad758397c211ab1406a0cf4
DESKTOP\Boss <david@gwynn-jones.com.au>
2018-05-29 14:45:02 +1000

    Azure media test initial load

---
+++ SurfSentry.Web/Controllers/SentryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurfSentry.Web.Controllers
+{
+    public class SentryController : Controller
+    {
+        //// GET: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
