e3114d82464e11ef5206c142e1808a9b6d2a13e5
DESKTOP\Boss <david@gwynn-jones.com.au>
2018-07-03 11:30:07 +1000

    Added Settings class

---
+++ SurfSentry.Web/Models/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SurfSentry.Web.Models
+{
+
+    public static class Settings
+    {
+        public static string ProductName
+        {
+            get
+            {
+                return "SurfSentry";
+            }
+            private set { }
+        }
+    }
+}
